7ffd66bb6ab2c9bbaffc875ec3d233f38d8f1db5
Unknown <thomas.wade@crownpeak.com>
2019-07-20 17:10:31 -0600

    adjust security groups

--- terraform/infrastructure/instances/security_groups.tf
+++ terraform/infrastructure/instances/security_groups.tf
@@ -4,24 +4,18 @@ resource "aws_security_group" "web" {
   vpc_id      = "${var.vpc-id}"
 
   egress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8333
+    to_port     = 8333
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 443
-    to_port     = 443
+  ingress {
+    from_port   = 8333
+    to_port     = 8333
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["${var.vpc-cidr-block}"]
+    security_groups = ["${aws_security_group.web-alb.id}"]
   }
 
   ingress {
@@ -31,11 +25,9 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["${var.vpc-cidr-block}"]
+  tags = {
+    environment = "${var.environment}"
+    name        = "web-${var.environment}"
   }
 }
 
@@ -45,24 +37,29 @@ resource "aws_security_group" "web-alb" {
   vpc_id      = "${var.vpc-id}"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8333
+    to_port     = 8333
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port       = 3000
-    to_port         = 3000
+    from_port       = 8333
+    to_port         = 8333
     protocol        = "tcp"
     cidr_blocks     = ["${var.vpc-cidr-block}"]
     security_groups = ["${aws_security_group.web.id}"]
+  }
+
+  egress {
+    from_port       = 8333
+    to_port         = 8333
+    protocol        = "tcp"
+    cidr_blocks     = ["${var.vpc-cidr-block}"]
+  }
+
+  tags = {
+    environment = "${var.environment}"
+    name        = "web-alb-${var.environment}"
   }
 }
