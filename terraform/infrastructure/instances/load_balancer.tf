c68ba163e557cd721e92255794d470428c1f67d7
Unknown <thomas.wade@crownpeak.com>
2019-07-20 21:30:23 -0600

    test nlb

--- terraform/infrastructure/instances/load_balancer.tf
+++ terraform/infrastructure/instances/load_balancer.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "web" {
   name            = "web-${var.environment}"
   internal        = false
   load_balancer_type = "network"
-  security_groups = ["${aws_security_group.web-alb.id}"]
+  # security_groups = ["${aws_security_group.web-alb.id}"]
   subnets         = ["${var.public-a-subnet-id}", "${var.public-b-subnet-id}"]
 
   tags = {
