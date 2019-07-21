8b47a2b74c0c482cbce12c455a5e76eb0d84eed3
Unknown <thomas.wade@crownpeak.com>
2019-07-20 20:22:24 -0600

    test cycle

--- terraform/infrastructure/instances/security_groups.tf
+++ terraform/infrastructure/instances/security_groups.tf
@@ -11,23 +11,17 @@ resource "aws_security_group" "web" {
   }
 
   ingress {
-    from_port   = 8333
-    to_port     = 8333
-    protocol    = "tcp"
-    # cidr_blocks = ["0.0.0.0/0"]
-    security_groups = ["${aws_security_group.web-alb.id}"]
-  }
-
-  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    environment = "${var.environment}"
-    # name        = "web-${var.environment}"
+  ingress {
+    from_port   = 8333
+    to_port     = 8333
+    protocol    = "tcp"
+    cidr_blocks = ["${var.vpc-cidr-block}"]
   }
 }
 
@@ -49,17 +43,5 @@ resource "aws_security_group" "web-alb" {
     protocol        = "tcp"
     cidr_blocks     = ["${var.vpc-cidr-block}"]
     security_groups = ["${aws_security_group.web.id}"]
-  }
-
-  # egress {
-  #   from_port       = 8333
-  #   to_port         = 8333
-  #   protocol        = "tcp"
-  #   cidr_blocks     = ["${var.vpc-cidr-block}"]
-  # }
-
-  tags = {
-    environment = "${var.environment}"
-    # name        = "web-alb-${var.environment}"
   }
 }
