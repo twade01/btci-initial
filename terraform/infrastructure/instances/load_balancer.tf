469a48a0d7558816633904a45321fa74cb4588e3
Unknown <thomas.wade@crownpeak.com>
2019-07-20 21:13:38 -0600

    change alb to lb

--- terraform/infrastructure/instances/load_balancer.tf
+++ terraform/infrastructure/instances/load_balancer.tf
@@ -1,8 +1,9 @@
 
-resource "aws_alb" "web" {
+resource "aws_lb" "web" {
   # count = "${var.count}"
   name            = "web-${var.environment}"
   internal        = false
+  load_balancer_type = "network"
   security_groups = ["${aws_security_group.web-alb.id}"]
   subnets         = ["${var.public-a-subnet-id}", "${var.public-b-subnet-id}"]
 
@@ -11,28 +12,28 @@ resource "aws_alb" "web" {
   }
 }
 
-resource "aws_alb_target_group" "web" {
+resource "aws_lb_target_group" "web" {
   name     = "web-target-group-${var.environment}"
   port     = "8333"
   protocol = "TCP"
   vpc_id   = "${var.vpc-id}"
 }
 
-resource "aws_alb_listener" "web" {
-  load_balancer_arn = "${aws_alb.web.arn}"
+resource "aws_lb_listener" "web" {
+  load_balancer_arn = "${aws_lb.web.arn}"
   port              = "8333"
   protocol          = "TCP"
 
   default_action {
-    target_group_arn = "${aws_alb_target_group.web.arn}"
+    target_group_arn = "${aws_lb_target_group.web.arn}"
     type             = "forward"
   }
 }
 
-resource "aws_alb_target_group_attachment" "web" {
+resource "aws_lb_target_group_attachment" "web" {
   count = "${var.countnum}"
 
-  target_group_arn = "${aws_alb_target_group.web.arn}"
+  target_group_arn = "${aws_lb_target_group.web.arn}"
   target_id        = "${element(aws_instance.web.*.id, count.index)}"
   port             = "8333"
 }
